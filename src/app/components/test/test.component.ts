55f623d6b6b5b7a5288c00273d3a3ee96384f52d
lilyVolo <lvolotchko@gmail.com>
2024-02-11 13:18:46 +0100

    toast notif, filter, timer

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Utils } from 'src/app/utils';
+
+// type Gender[] = ['male', 'female', 'unknown'];
+type GenderColors = { [key: string]: string };
 
 interface Pokemon {
   name: string;
@@ -11,8 +15,19 @@ interface Pokemon {
   styleUrls: ['./test.component.sass'],
 })
 export class TestComponent {
+  addedPokemon = '';
+
   newPokemonName = '';
 
+  duplicatePokemon = '';
+
+  genderColors: GenderColors = {
+    male: 'blue',
+    female: 'pink',
+    unknown: 'purple',
+  };
+
+  genders = ['male', 'female', 'unknown'];
   pokemons: Pokemon[] = [
     {
       name: 'Pikachu',
@@ -24,18 +39,46 @@ export class TestComponent {
     },
   ];
 
-  addPockemon() {
-    const newPokemon = {
+  pokemonAlreadyExists(pokemonName: string) {
+    const pokemonWithSameName = this.pokemons.find((pokemon) => {
+      return pokemonName === pokemon.name;
+    });
+    return pokemonWithSameName !== undefined;
+  }
+
+  addPokemon() {
+    if (this.pokemonAlreadyExists(this.newPokemonName)) return;
+    const newPokemon: Pokemon = {
       name: this.newPokemonName,
-      gender: 'unknown',
+      gender: this.getRandomGender(),
     };
     this.pokemons.unshift(newPokemon);
+    this.addedPokemon = this.newPokemonName;
     this.newPokemonName = '';
+    this.closeToastAfterSomeTime();
   }
 
   onInputKeyPress(event: KeyboardEvent) {
     if (event.code === 'Enter') {
-      this.addPockemon();
+      this.addPokemon();
     }
+  }
+
+  getRandomGender() {
+    const randomNumberGender = Utils.getRandomNumber(
+      0,
+      this.genders.length - 1
+    );
+    return this.genders[randomNumberGender];
+  }
+
+  closeToastAfterSomeTime() {
+    setTimeout(() => {
+      this.onToastClose();
+    }, 2000);
+  }
+  onToastClose() {
+    this.addedPokemon = '';
+    this.duplicatePokemon = '';
   }
 }
